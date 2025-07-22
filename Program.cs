a125f9759ada98632e0c41f4adcdd25b6b724603
Marcos Vinicius Rech <vini_apolo@hotmail.com>
2025-07-21 22:29:18 -0300

    Projeto POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando Nokia:");
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "1111111111", memoria: 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Testando iPhone:");
+        Smartphone iphone = new Iphone(numero: "654321", modelo: "iPhone 13", imei: "2222222222", memoria: 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
